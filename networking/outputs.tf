fc6693cef427d726af4717587a9bd82d6d4b02ba
dan ruyle <dan.ruyle@tylertech.com>
2025-10-19 20:54:43 -0400

    output hosted zone id if created

--- networking/outputs.tf
+++ networking/outputs.tf
@@ -1,3 +1,7 @@
 output "acm_ssl_certificate_arn" {
   value = var.use_existing_ssl_certificate ? data.aws_acm_certificate.existing_ssl_certificate[0].arn : aws_acm_certificate.ssl_certificate[0].arn
 }
+
+output "route_53_hosted_zone_id" {
+  value = local.use_route_53 ? local.hosted_zone_id : null
+}
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
 output "acm_ssl_certificate_arn" {
   value = module.networking.acm_ssl_certificate_arn
 }
+
+output "route_53_hosted_zone_id" {
+  value = module.networking.route_53_hosted_zone_id
+}
