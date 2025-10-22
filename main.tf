851820c7c51e9ba2f6a936e3b8aed9449d7929ee
ruyda <dan.ruyle@tylertech.com>
2025-10-22 18:19:06 -0400

    cache behavior & origin

--- main.tf
+++ main.tf
@@ -6,9 +6,11 @@ module "s3_static_website" {
 }
 
 module "networking" {
-  depends_on                   = [ module.s3_static_website ]
-  source                       = "./networking"
-  domain                       = var.domain
-  dns_type                     = var.dns_type
-  use_existing_ssl_certificate = var.use_existing_ssl_certificate
+  depends_on                     = [ module.s3_static_website ]
+  source                         = "./networking"
+  domain                         = var.domain
+  dns_type                       = var.dns_type
+  use_existing_ssl_certificate   = var.use_existing_ssl_certificate
+  distribution_caching_policy_id = var.distribution_caching_policy_id
+  origin_request_policy_id       = var.origin_request_policy_id
 }
